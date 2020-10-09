eb8b925cdb049928c455041dcfb006a4d23a869c
Amina Safdar <asafdarali@gmail.com>
2020-10-08 22:30:30 -0400

    adding files

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+const dotenv = require("dotenv");
+
+if (process.env.ENVIRONMENT !== "production") {
+  dotenv.config();
+}
+
+if (process.env.NODE_ENV === 'development') {
+  process.env.GATSBY_WEBPACK_PUBLICPATH = '/'
+}
+
+const {
+  spaceId,
+  accessToken
+} = process.env;
+
+module.exports = {
+  plugins: [{
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId,
+        accessToken
+      }
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    }
+  ]
+};
